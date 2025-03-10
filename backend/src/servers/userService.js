743be67329d12c8857eeaa666c0c2410fe32f4d5
Gerlandio <gerlandios51@gmail.com>
2025-03-10 09:35:37 -0300

    feat: criando atributo foto de perfil

--- backend/src/servers/userService.js
+++ backend/src/servers/userService.js
@@ -13,6 +13,7 @@ export const createUser = async (newUser)=>{
             name: newUser.name,
             password: newUser.password,
             user: newUser.user,
+            profilePicture: newUser.profilePicture,
             messagesArray:{
                 create: []
             }
